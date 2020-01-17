ce69bd0a0607aca3f39758b6994103e16ae04af6
m80b33 <31405250+m80b33@users.noreply.github.com>
2020-01-17 12:43:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,20 +6,27 @@ from collections import Counter
 import tkinter as tk
 from tkinter import filedialog
 
+from datetime import datetime
+
 
 def isadmin():
     try:
-        iadmin = (os.getuid() == 0)
-    except AttributeError:
-        iadmin = ctypes.windll.shell32.IsUserAnAdmin() != 0
-    return iadmin
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
 
 
 def fileopen():
     root = tk.Tk()
     root.withdraw()
-    filepath = filedialog.askopenfilename()
+    filepath = filedialog.askopenfilename(filetype=[('CSV File', '*.csv')])
     return filepath
+
+
+def loggin(fromlist, tolist):
+    with open('C:\\blockIP.log', 'a') as file:
+        file.write('\n{}\n--------------------\nIP in list:\n{}\n--------------------\nNew IP added:\n{}\n--------------------\n'.format(str(datetime.now()).split('.')[0], fromlist, tolist))
+        file.close()
 
 
 def iplistcsv(filename, ip):
@@ -43,11 +50,13 @@ def iplistreg(filename):
         fulllist = regipl + fromcsv
         list = ''.join( str(ip) + ',' for ip in fulllist)
         os.system(r'netsh advfirewall firewall set rule name="{}" new remoteip="{}"'.format(str(rulename), list))
+        loggin(regipl, fromcsv)
         ctypes.windll.user32.MessageBoxW(0, 'Done!\nThe Rule \'blockIP\' successfully updated!', 'IPBlocker', 64)
 
     except subprocess.CalledProcessError:
         list = ''.join( str(ip) + ',' for ip in fromcsv)
         os.system(r'netsh advfirewall firewall add rule name="{}" action=block dir=IN remoteip="{}"'.format(str(rulename), list))
+        loggin(fromlist='The Rule \'blockIP\' successfully created!', tofile=fromcsv)
         ctypes.windll.user32.MessageBoxW(0, 'Done!\nThe Rule \'blockIP\' successfully created, ips from log file was added!', 'IPBlocker', 64)
 
 
@@ -58,6 +67,9 @@ if __name__ == '__main__':
         if os.path.exists(filename):
             iplistreg(filename)
         else:
-            print('File not selected. Bye!')
+            errmsg = 'File not selected!'
+            loggin(fromlist=errmsg, tolist=errmsg)
+            ctypes.windll.user32.MessageBoxW(0, errmsg, 'IPBlocker', 16)
     else:
-        ctypes.windll.user32.MessageBoxW(0, 'You should run this application as an Administrator!', 'IPBlocker', 16)
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+
